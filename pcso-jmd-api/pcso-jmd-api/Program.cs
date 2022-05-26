c3fb155fd6b3137fbeef09d2adee40f2be6f4c29
Jonathan Deabanico <jdeabanico@pcso.gov.ph>
2022-05-26 10:14:26 +0800

    Implemented Product Delete

--- pcso-jmd-api/pcso-jmd-api/Program.cs
+++ pcso-jmd-api/pcso-jmd-api/Program.cs
@@ -11,6 +11,7 @@ app.MapGet("Hello", () => "This is very cool I Hope");
 app.MapGet("Product", () => "Get some product in Azure SQL");
 
 app.MapPost("Product", (Product p) => "Product saved in SQL");
+app.MapDelete("Product", (string Name) => "Deleted");
 app.MapPost("Chat", (Message msg) => "Message posted to chat SQL");
 
 
