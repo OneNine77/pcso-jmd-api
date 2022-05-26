5e437c54745925d56bf4d30ecec41fc82b551189
Jonathan Deabanico <jdeabanico@pcso.gov.ph>
2022-05-26 13:59:46 +0800

    Implemented to do api

--- pcso-jmd-api/pcso-jmd-api/Program.cs
+++ pcso-jmd-api/pcso-jmd-api/Program.cs
@@ -1,31 +1,111 @@
-using pcso_jmd_api;
+//using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//builder.Services.AddSignalR();
+
+//builder.Services.AddCors();
 
 var app = builder.Build();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
 
-MessageManager messageManager = new MessageManager();
+//app.MapHub<Chat>(nameof(Chat));
 
-app.MapGet("Hello", () => "This is very cool I Hope");
-app.MapGet("Product", () => "Get some product in Azure SQL");
+app.MapGet("/", () => "Hello World!");
 
-app.MapPost("Product", (Product p) => "Product saved in SQL");
-app.MapDelete("Product", (string Name) => "Deleted");
+app.MapGet("/todoitems", async (TodoDb db) =>
+    await db.Todos.ToListAsync());
 
-app.MapGet("Chat", () => messageManager?.Messages);
-app.MapPost("Chat", (Message msg) => messageManager.Messages.Add(msg));
+app.MapGet("/todoitems/complete", async (TodoDb db) =>
+    await db.Todos.Where(t => t.IsComplete).ToListAsync());
 
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+    await db.Todos.FindAsync(id)
+        is Todo todo
+            ? Results.Ok(todo)
+            : Results.NotFound());
+
+app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+{
+    todo.Created = DateTime.Now;
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
+
+app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.Ok(todo);
+    }
+
+    return Results.NotFound();
+});
+
+app.MapDelete("/todoitems", async (TodoDb db) =>
+{
+    await db.Database.EnsureDeletedAsync();
+    await db.SaveChangesAsync();
+    return Results.Ok(null);
+});
+
+//app.UseCors(builder => builder
+//         .AllowAnyHeader()
+//         .AllowAnyMethod()
+//         .SetIsOriginAllowed((host) => true)
+//         .AllowCredentials()
+//     );
 
 app.Run();
 
-//Some data contracts
+//public class Chat : Hub
+//{
+//    public void Broadcast(string name, string message)
+//    {
+//        Clients.All.SendAsync("Receive", name, message);
+//    }
+//}
 
-public record class Message(string Name, string Msg);
-public record class Product(string Name, string Picture, int Price);
+class Todo
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+    public string? Message { get; set; }
+    public DateTime? Created { get; set; }
+}
 
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
 
